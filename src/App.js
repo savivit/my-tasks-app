7860c418b26e0a43433e1ecd89e0a0840de43f79
savivit <savivit@gmail.com>
2020-01-25 12:08:24 +0200

    pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { Switch, Route } from 'react-router-dom'
-import HomePage from './pages/HomePage';
+import HomePage from './Pages/HomePage';
 import LoginPage from './pages/LoginPage';
-import RecipesPage from './pages/RecipesPage';
+import TasksPage from './pages/TasksPage';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
@@ -12,73 +12,73 @@ class App extends React.Component {
 
   constructor(props) {
     super(props);
-    
+
     this.state = {
-      activeUser: null,
-      allUsers: jsonUsers,
-      allRecipes: jsonRecipes
-      // allRecipes: jsonRecipes.map(recipe => new RecipeModel(recipe));
-      // {
-      //   "id": "2121212",
-      //   "name": "Nir"
+      //   activeUser: null,
+      //   allUsers: jsonUsers,
+      //   allRecipes: jsonRecipes
+      //   // allRecipes: jsonRecipes.map(recipe => new RecipeModel(recipe));
+      //   // {
+      //   //   "id": "2121212",
+      //   //   "name": "Nir"
+      //   // }
+
+      //   //null
       // }
 
-      //null
+      // this.handleLogout = this.handleLogout.bind(this);
+      // this.handleLogin = this.handleLogin.bind(this);
+      // this.handleNewRecipe = this.handleNewRecipe.bind(this);
     }
 
-    this.handleLogout = this.handleLogout.bind(this);
-    this.handleLogin = this.handleLogin.bind(this);
-    this.handleNewRecipe = this.handleNewRecipe.bind(this);
-  }
-  
-  handleLogin(user) {
-    this.setState({
-      activeUser: user
-    });
-  }
+    // handleLogin(user) {
+    //   this.setState({
+    //     activeUser: user
+    //   });
+    // }
 
-  handleLogout() {
-    this.setState({
-      activeUser: null
-    })
-  }
+    // handleLogout() {
+    //   this.setState({
+    //     activeUser: null
+    //   })
+    // }
 
-  handleNewRecipe(newRecipe) {
-    const { allRecipes, activeUser } = this.state;
+    // handleNewRecipe(newRecipe) {
+    //   const { allRecipes, activeUser } = this.state;
 
-    // adding one to the id of the last recipe in the array
-    newRecipe.id = allRecipes[allRecipes.length - 1].id + 1;
-    newRecipe.userId = activeUser.id;
+    //   // adding one to the id of the last recipe in the array
+    //   newRecipe.id = allRecipes[allRecipes.length - 1].id + 1;
+    //   newRecipe.userId = activeUser.id;
 
-    this.setState({
-      allRecipes: allRecipes.concat(newRecipe)
-    })
+    //   this.setState({
+    //     allRecipes: allRecipes.concat(newRecipe)
+    //   })
   }
 
 
   render() {
-    const { activeUser, allUsers, allRecipes } = this.state;
+    // const { activeUser, allUsers, allRecipes } = this.state;
 
-    const activeUserRecipes = activeUser ? 
-      allRecipes.filter(recipe => recipe.userId === activeUser.id) : null
+    // const activeUserRecipes = activeUser ?
+    //   allRecipes.filter(recipe => recipe.userId === activeUser.id) : null
 
-    return ( 
+    return (
 
       <Switch>
         <Route exact path="/">
-          <HomePage activeUser={activeUser} handleLogout={this.handleLogout}/>
+          <HomePage activeUser={activeUser} handleLogout={this.handleLogout} />
         </Route>
         <Route exact path="/login">
-          <LoginPage allUsers={allUsers} handleLogin={this.handleLogin}/>
+          <LoginPage allUsers={allUsers} handleLogin={this.handleLogin} />
         </Route>
-        <Route exact path="/recipes">
-          <RecipesPage activeUser={activeUser} recipes={activeUserRecipes} handleLogout={this.handleLogout} 
-            handleNewRecipe={this.handleNewRecipe}/>
+        <Route exact path="/tasks">
+          <TasksPage activeUser={activeUser} recipes={activeUserRecipes} handleLogout={this.handleLogout}
+            handleNewRecipe={this.handleNewRecipe} />
         </Route>
 
       </Switch>
     );
-  
+
   }
 }
 
@@ -86,7 +86,7 @@ export default App;
 
 
 
-
+/*
 function App() {
   return (
     <div className="App">
@@ -98,3 +98,4 @@ function App() {
 }
 
 export default App;
+ */
